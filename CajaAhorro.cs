c4d6f9e5c8263e327fe32ea8d64e55ab8c721b33
MarianParisi <marianparisi@gmail.com>
2017-10-29 20:39:38 -0300

    nuevas clases

---
+++ CajaAhorro.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Banco
+{
+    public class CajaAhorro : Cuenta
+    {
+  
+    // public override decimal Saldo {get; set;}
+    //return decimal positivo?
+    
+    }
+}
---
+++ CuentaCorriente.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Banco
+{
+    public class CuentaCorriente : Cuenta
+    {
+  
+    // public override decimal Saldo {get; set;}
+    //return decimal 
+    
+    }
+}
