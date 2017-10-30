88dbd2b87ec78bd3ea8cf31f05e0ca00e99b3425
MarianParisi <marianparisi@gmail.com>
2017-10-30 19:24:24 -0300

    nuevos cambios

--- Cuenta.cs
+++ Cuenta.cs
@@ -6,16 +6,28 @@ namespace Banco
     {
     public long CBU {get; set;}
     public decimal Saldo {get; set;}
-    
-    public List<string> titulares { get; set; }
+    // en caso de que hay un solo titular
+    // public Cliente Titular {get; set;}
+    public List<Cliente> Titulares { get; set; }
     public decimal depositar ( decimal montoDeposito){
-        Saldo = Saldo + montoDeposito;
-        return Saldo;
+       /* Saldo = Saldo + montoDeposito;
+        return Saldo;*/
+        this.Saldo += montoDeposito;
+        return this.Saldo;
     }
     public virtual decimal retirar ( decimal montoDeposito){
-        Saldo = Saldo - montoDeposito;
-        return Saldo;
+
+       /* Saldo = Saldo - montoDeposito;
+        return Saldo;*/
+
+        this.Saldo -= montoDeposito;
+        return this.Saldo;
     }
     
+    public Cuenta()
+    {
+        this.Saldo=0;
+        this.Titulares= new List<Cliente>();
+    }
     }
 }
--- Program.cs
+++ Program.cs
@@ -9,24 +9,31 @@ namespace Banco
         {
             Cliente Cliente1 = new Cliente ();
             Cliente1.nombreCompleto= "Carlos Perez";
-            Cliente1.DNI= 25432170;
+            Cliente1.DNI= 30432170;
             Cliente1.Telefono= 46876770;
+            Cliente1.FechaDeNacimiento= new DateTime (1983, 1, 2);
             
             Cliente Cliente2 = new Cliente ();
             Cliente2.nombreCompleto= "juan garzia";
             Cliente2.DNI= 30256180;
             Cliente2.Telefono= 46844770;
+            Cliente2.FechaDeNacimiento= new DateTime (1980, 11, 12);
 
             Cuenta NuevaCuenta = new Cuenta ();
             NuevaCuenta.CBU=11110000055;
             NuevaCuenta.Saldo=2000;
-            NuevaCuenta.titulares= new List<string> 
-            {Cliente1.nombreCompleto, Cliente2.nombreCompleto};
-
+           // NuevaCuenta.Titulares.AddRange(Cliente1, Cliente2)?
+            NuevaCuenta.Titulares.Add(Cliente1);
+            NuevaCuenta.Titulares.Add(Cliente2);
+             
             Console.WriteLine ("El saldo de tu cuenta es: " + NuevaCuenta.retirar (1500));
             
             Console.WriteLine ("El saldo de tu cuenta es: " + NuevaCuenta.depositar (200));
             
+            foreach (Cliente titular in NuevaCuenta.Titulares)
+            {
+                Console.WriteLine(titular);
+            }
         }
     }
-}
+}// f11 para debuguear por linea y meterse adentro. f10 para salir.
